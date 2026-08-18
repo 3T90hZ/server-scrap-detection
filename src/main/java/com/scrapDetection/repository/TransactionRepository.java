9ef24e02bd99dbf564f69c07e5527454f44c717d
3T90hZ <tienthanh6042@gmail.com>
2026-08-18 17:54:32 +0700

    Remove deprecated services

--- src/main/java/com/scrapDetection/repository/TransactionRepository.java
+++ src/main/java/com/scrapDetection/repository/TransactionRepository.java
@@ -2,24 +2,11 @@ package com.scrapDetection.repository;
 
 import com.scrapDetection.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-    List<Transaction> findByCustomerAccountId(Long customerId);
-
-    @Query("SELECT t FROM Transaction t WHERE t.createdBy.accountId = :staffId")
-    List<Transaction> findByCreated_byAccountId(@Param("staffId") Long staffId);
-
     List<Transaction> findByMaterialMaterialId(Long materialId);
-
-    List<Transaction> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
-
-    List<Transaction> findByMaterialScrapYardYardId(Long yardId);
 }
