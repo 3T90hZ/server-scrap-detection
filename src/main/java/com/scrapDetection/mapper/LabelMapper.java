b453e06e580757bf195d884ae0c9caff386d8237
3T90hZ <tienthanh6042@gmail.com>
2026-08-21 16:02:43 +0700

    Add label mapper

---
+++ src/main/java/com/scrapDetection/mapper/LabelMapper.java
@@ -0,0 +1,33 @@
+package com.scrapDetection.mapper;
+
+import com.scrapDetection.dto.device.DeviceRequestDTO;
+import com.scrapDetection.dto.device.DeviceResponseDTO;
+import com.scrapDetection.dto.label.LabelRequest;
+import com.scrapDetection.dto.label.LabelResponse;
+import com.scrapDetection.entity.Device;
+import com.scrapDetection.entity.Label;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class LabelMapper {
+
+    public LabelResponse toResponseDTO(Label entity) {
+        if (entity == null) return null;
+
+        return LabelResponse.builder()
+                .labelId(entity.getLabelId())
+                .label(entity.getLabel())
+                .materialId(entity.getMaterial().getMaterialId())
+                .materialName(entity.getMaterial().getItemName())
+                .build();
+    }
+
+    public List<LabelResponse> toResponseDTOList(List<Label> labels) {
+        return labels.stream()
+                .map(this::toResponseDTO)
+                .collect(Collectors.toList());
+    }
+}
