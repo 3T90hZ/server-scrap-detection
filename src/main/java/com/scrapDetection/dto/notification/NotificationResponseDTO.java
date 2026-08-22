50fff986c4263c45eb9f5dc4d19835a01e136a0e
3T90hZ <tienthanh6042@gmail.com>
2026-08-23 04:31:44 +0700

    Create notification response DTO

---
+++ src/main/java/com/scrapDetection/dto/notification/NotificationResponseDTO.java
@@ -0,0 +1,28 @@
+package com.scrapDetection.dto.notification;
+
+import com.scrapDetection.entity.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationResponseDTO {
+    private Long notificationId;
+
+    private String title;
+
+    private String message;
+
+    private NotificationType type;
+
+    private Boolean isRead;
+
+    private LocalDateTime createdAt;
+
+    private Long billId;
+}
